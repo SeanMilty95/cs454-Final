c58c33f735d92dda85ecf9a060a019201861ef9f
JosephHaun <riddlemethis565@gmail.com>
2019-05-15 12:44:33 -0700

    Fixed slight errors in BFS function and added comments

--- functions.py
+++ functions.py
@@ -60,36 +60,35 @@ def GetSmallestPalindrome(DFA, M):
 
 def breadth_first_search(DFA): #Requires testing with whatever function that is working 
     #Function takes in a DFA, M, and returns a string that is accepted and of minimum length
-    ToVisit = queue.Queue(0);
-    Visited = set()
+    ToVisit = queue.Queue(0); #Setup queue of states to be visited
+    Visited = set() #Setup queue of states visited, initially empty
 
-    Info = dict();
+    Info = dict(); #Information about the state
 
-    Begin = M.report_current_state();
-    Info[Begin] = (None, None)
-    ToVisit.put(Begin)
-    Count = 0;
-    inputList = list();
-    while not ToVisit.empty():
-        Cur = ToVisit.get();
+    Begin = DFA.report_current_state(); #Get current state in the DFA, should be first state if initialized properly
+    Info[Begin] = (None, None) #Enter none for beginning state
+    ToVisit.put(Begin) #Place Begin on ToVisit queue
+    Count = 0; #Set Count to zero
+    inputList = list(); #Setup inputList to empty
+    while not ToVisit.empty(): #While there are still elements to visit
+        Cur = ToVisit.get(); #Get next queue element
 
-        if M.in_accept_state:
+        if M.in_accept_state: #If in accept state place values onto inputList and then return it
             while(Info[Cur][0] is not None):
                   Cur, inputVal = Info[Cur];
                   print(inputVal);
                   inputList.append(inputVal);
             return inputList.reverse();
 
-        for (Next, InputVal) in M.get_next_states():
+        for (Next, InputVal) in DFA.get_next_states(): #Get next states and place onto ToVisit queue if not previously visited
             if Next in Visited:
                 continue
             if Next not in ToVisit:
                 Info[Next] = (Cur, InputVal);
                 ToVisit.put(Next);
-        Count = Count + 1;
+        Count = Count + 1; #Increment count to keep moving through the DFA
         Visited.add(Cur);
-    print("No solution")
-    return inputList;
+        return inputList; #Return the inputList queue regardless of what is in it
     
 #This function takes a list and adds one to the appropriate place
 def change_number_value(index, num_list):
